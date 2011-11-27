547495732e28de6c7ee36432f3b2b0d604004616
Matt Aimonetti <mattaimonetti@gmail.com>
2011-11-27 20:51:42 -0300

    Initial Commit

---
+++ TVFrancaise/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  TVFrancaise
+#
+#  Created by Matt Aimonetti on 11/27/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+  attr_accessor :window
+  def applicationDidFinishLaunching(a_notification)
+    # Insert code here to initialize your application
+  end
+end
+
---
+++ TVFrancaise/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TVFrancaise
+//
+//  Created by Matt Aimonetti on 11/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+  return macruby_main("rb_main.rb", argc, argv);
+}
