df42f33e1d3906440998670c178222c3278f7eca
Matt Aimonetti <mattaimonetti@gmail.com>
2011-11-28 21:11:59 -0300

    cleaned up for GitHub Release

---
+++ README.md
@@ -0,0 +1,40 @@
+= TV Française
+
+Simple OS X application allowing you to watch most of the free French TV
+channels live. (works overseas)
+
+This product is buggy, mainly for educational purposes and certainly not
+maintained. Use it at your own risks.
+
+== Details
+
+The application is a native OS X Cocoa application written in MacRuby
+http://macruby.org You can learn more about MacRuby via my book: http://amzn.to/tVx4ng
+
+The code is very simple and straight forward (currently under 100 LOC).
+If you decide to help the project, check the TODO list and learn more
+about the following Cocoa classes:
+
+* NSOutlineView
+* QTMovieView
+* QTMovieView
+* NSProgressIndicator
+
+== TODO list
+
+* Full screen mode (in the meantime, maximize the window and close the
+  outline view).
+* EPG support (to know what's being broadcast, what's coming up next..)
+* Recording (not sure QTMovie allows that. Scheduled recordings might also be nice)
+* Channel updates via internet
+* Custom channels (add your own feeds)
+* Real time chat (for whoever wants to play with Node.js + Heroku)
+* Screenshot
+* Multiple simultaneous players (watch 2 soccer games at the same time)
+* Misc hacks
+* ad supported version so I can become super rich and move back to France (j/k)
+
+== Thanks
+
+Thanks to Patrick Crowley for the current icon, hopefully, we'll come up
+with something better ;)
--- TVFrancaise/AppDelegate.rb
+++ TVFrancaise/AppDelegate.rb
@@ -3,13 +3,107 @@
 #  TVFrancaise
 #
 #  Created by Matt Aimonetti on 11/27/11.
-#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#  Copyright 2011
 #
 
 class AppDelegate
   attr_accessor :window
+  attr_accessor :outline
+  attr_accessor :player
+  attr_accessor :spinner
+  
   def applicationDidFinishLaunching(a_notification)
-    # Insert code here to initialize your application
   end
+  
+  def awakeFromNib
+    @spinner.displayedWhenStopped = false
+    @player.hidden = true
+    @data = [{:group => 'Chaines Françaises', 
+      :child => [
+      {"France 2" => "http://94.247.234.2/streaming/francetv_ft2/ipad.m3u8"},
+      {"France 3" => "http://94.247.234.2/streaming/francetv_ft3/ipad.m3u8"},
+      {"France 4" =>  "http://94.247.234.2/streaming/francetv_ft4/ipad.m3u8"},
+      {"France 5" => "http://94.247.234.4/streaming/francetv_ft5/ipad.m3u8"},
+      {"France Ô" => "http://94.247.234.4/streaming/francetv_fto/ipad.m3u8"},
+      {"M6" => "http://m6-hls-live.adaptive.level3.net/apple/m6replay_iphone/m6live/m6live_ipad.m3u8"},
+      {"W9" => "http://m6-hls-live.adaptive.level3.net/apple/m6replay_iphone/m6live/w9live.m3u8"},
+      {"NRJ12" => "http://nrj-apple-live.adaptive.level3.net/apple/nrj/nrj/nrj12.m3u8"},
+      {"Direct Star" => "http://cupertino-streaming-1.hexaglobe.com/rtpdirectstarlive/smil:directstar-ipad.smil/playlist.m3u8"},
+      {"France 24" => "http://stream7.france24.yacast.net/iphone/france24/fr/iPad.f24_fr.m3u8"},
+      {"BFM TV" => "http://http5.iphone.yacast.net/iphone/bfmtv/bfmtv_ipad.m3u8"},
+      {"BFM Business" => "http://stream7.bfmbiz.yacast.net/iphone/bfmbiz/bfmbiz_live01.m3u8"},
+      {"NRJ Pop Rock" => "http://nrjlive-apple-live.adaptive.level3.net/apple/nrj/nrjlive-4/appleman.m3u8"},
+      {"NRJ Pure" => "http://nrjlive-apple-live.adaptive.level3.net/apple/nrj/nrjlive-3/appleman.m3u8"},
+      {"NRJ Dance" => "http://nrjlive-apple-live.adaptive.level3.net/apple/nrj/nrjlive-2/appleman.m3u8"},
+      {"NRJ Urban" => "http://nrjlive-apple-live.adaptive.level3.net/apple/nrj/nrjlive-1/nrjurban.m3u8"}
+      ] }]
+    outline.expandItem(@data[0])
+    stream_channel(13)
+  end
+  
+  def outlineView(outlineView, child: index, ofItem: item)
+    return nil if @data.nil?
+    return @data[index] if item.nil?
+    return item[:child][index].keys.first
+  end
+  
+  def outlineView(outlineView, numberOfChildrenOfItem:item)
+    return @data.size if item.nil?
+    return item[:child].size
+  end
+  
+  def outlineView(outlineView, isItemExpandable:item)
+    item.kind_of?(Hash)
+  end
+  
+  def outlineView(outlineView, objectValueForTableColumn:tableColumn, byItem:item)
+    item.kind_of?(Hash) ? item[:group] : item
+  end
+  
+  def windowWillClose(sender); exit(1); end
+  
+=begin
+  def outlineView(outlineView, shouldSelectItem:item)
+    puts "#{item} selected"
+    return false if item.kind_of?(Hash)
+    return true
+  end
+=end
+  
+  def outlineViewSelectionDidChange(notification)
+    row = outline.selectedRow
+    if row > -1
+      stream_channel(row-1)
+    end
+  end
+  
+  def loading?(movie)
+    puts "loading..." if movie.attributeForKey(QTMovieLoadStateAttribute) == QTMovieLoadStateLoading
+  end
+  
+  def stream_channel(idx)
+    unless @last_idx == idx
+      @spinner.startAnimation(nil)
+      value = @data.first[:child][idx].values.first
+      url = NSURL.URLWithString(value)
+      puts "Changing channel"
+      error = Pointer.new("@")
+      movie = QTMovie.alloc.initWithAttributes({QTMovieOpenForPlaybackAttribute => true, QTMovieURLAttribute => url}, error)
+      @loading_check_thread.exit if @loading_check_thread
+      @loading_check_thread = Thread.new do
+        @player.hidden = true unless @player.movie
+        while(movie.attributeForKey(QTMovieLoadStateAttribute) == QTMovieLoadStateLoading) do
+          puts "loading..."
+          sleep(1) 
+        end
+        movie.autoplay
+        @player.hidden = false
+        @spinner.stopAnimation(nil)
+        @player.setMovie(movie)
+      end
+      @last_idx = idx
+    end
+  end
+  
 end
 
